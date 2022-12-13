442186f0968cd4f9c2d06f2f41d0b984e129500b
Dita Widayanti Setiawan <115095585+ditawys@users.noreply.github.com>
2022-12-13 05:45:31 -0800

    Add files via upload

---
+++ .idea/main.py
@@ -0,0 +1,17 @@
+import numpy as python
+
+#Dita Widayanti Setiawan
+#F55121098
+sumofmultipe = []
+Y = 3
+while Y < 20:
+    if Y % 3 == 0:
+        sumofmultipe.append(Y)
+    elif Y & 5 == 0:
+        sumofmultipe.append(Y)
+    Y = Y + 3
+print (sumofmultipe)
+var = 0
+for i in sumofmultipe:
+    var = var + i
+print (var)
